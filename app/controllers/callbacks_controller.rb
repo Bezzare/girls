703e9288234c50433316b584a3eb24d4396555b2
Bezzare <bezzarelin@gmail.com>
2015-10-02 18:08:28 +0800

    refactor duplicated methods

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,11 +1,9 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-    def facebook
+    def social_login
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in_and_redirect @user
     end
 
-    def google_oauth2
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in_and_redirect @user
-    end
+    alias_method :facebook, :social_login
+    alias_method :google_oauth2, :social_login
 end
