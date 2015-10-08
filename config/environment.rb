0aefa7ef95e2da01b3fe5b5592391ebb5f07bc2b
Bezzare <bezzarelin@gmail.com>
2015-10-08 13:15:37 +0800

    move secrets into secrets.yml

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-YAML.load_file("#{::Rails.root}/config/provider.yml")[::Rails.env].each {|k,v| ENV[k] = v }
 
 # Initialize the Rails application.
 Rails.application.initialize!
